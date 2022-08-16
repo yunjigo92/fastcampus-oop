fc90962671530979bddb659a5a251781a6487cb3
yunji <60842443+yunjigo92@users.noreply.github.com>
2022-08-16 10:30:48 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3은 여기서 마무리하고 스프링으로 전환

--- src/main/java/com/pastcampus/javaoop/Main.java
+++ src/main/java/com/pastcampus/javaoop/Main.java
@@ -1,6 +1,7 @@
 package com.pastcampus.javaoop;
 
 import com.pastcampus.javaoop.logic.BubbleSort;
+import com.pastcampus.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
@@ -13,7 +14,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new BubbleSort<>();
         System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
 }
